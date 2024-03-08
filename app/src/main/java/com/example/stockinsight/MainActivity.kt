c1a79a87b8b60539112cf0ef58828be3f077f72c
tvhungpv <tranviethungpv@gmail.com>
2024-03-08 14:22:33 +0700

    SplashScreen and OnboardingScreen

--- app/src/main/java/com/example/stockinsight/MainActivity.kt
+++ app/src/main/java/com/example/stockinsight/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // display onboarding fragment
+
+        val onboardingGetStartedFragment = OnboardingGetStartedFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, onboardingGetStartedFragment).commit()
     }
 }

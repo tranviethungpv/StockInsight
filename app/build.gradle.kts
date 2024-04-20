8277371813b77283ef88a86006c60dd044b1638c
tvhungpv <tranviethungpv@gmail.com>
2024-04-20 21:27:16 +0700

    Update Gradle and Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,14 +45,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.benchmark:benchmark-common:1.2.3")
+    implementation("androidx.benchmark:benchmark-common:1.2.4")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
     implementation("junit:junit:4.13.2")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
@@ -60,8 +60,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-android-compiler:2.51.1")
-    implementation("com.squareup.retrofit2:retrofit:2.10.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
